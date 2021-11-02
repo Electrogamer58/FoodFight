a89e9353d9c6e2a4ce1bc7f6523988848aa5325f
Daniel <dancoches@gmail.com>
2021-11-02 18:53:51 -0500

    State Setup

---
+++ Assets/_Game/Scripts/StateMachines/BattleGameSM/Base/BattleGameSM.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleGameSM : StateMachine
+{
+    [SerializeField] InputController _input;
+    public InputController Input => _input;
+
+    void Start()
+    {
+        ChangeState<SetupBattleState>();
+    }
+
+}
---
+++ Assets/_Game/Scripts/StateMachines/BattleGameSM/Base/BattleGameState.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(BattleGameSM))]
+public class BattleGameState : State
+{
+   protected BattleGameSM StateMachine { get; private set; }
+
+    private void Awake()
+    {
+        StateMachine = GetComponent<BattleGameSM>();
+    }
+}
