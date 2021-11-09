7640a27150e644011b7ce48905a2387f2c73e57c
Daniel <dancoches@gmail.com>
2021-11-09 15:42:40 -0600

    Commands Added

---
+++ Assets/_Game/Scripts/FF_Commands/Attack_Player.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack_Player : ICommand
+{
+    Animation _animation;
+    int _rolledNumber;
+    int _rollToHit;
+    int _damageToDeal;
+    public static event Action HitPlayer;
+    public static event Action MissedPlayer;
+
+    public Attack_Player(Animation animation, int rolledNumber, int rollToHit, int damageToDeal)
+    {
+        _animation = animation;
+        _rolledNumber = rolledNumber;
+        _rollToHit = rollToHit;
+        _damageToDeal = damageToDeal;
+    }
+
+    public void Execute()
+    {
+        Debug.Log("Tried Attack Player!");
+        if (_animation != null)
+            _animation.Play();
+
+        if (_rolledNumber >= _rollToHit)
+        {
+            HitPlayer?.Invoke();
+        }
+        if (_rolledNumber < _rollToHit)
+        {
+            MissedPlayer?.Invoke();
+        }
+
+    }
+
+    public void Undo()
+    {
+        Debug.Log("Stopped Attacking Player");
+        if (_animation != null)
+        {
+            if (_animation.isPlaying)
+                _animation.Stop();
+        }
+    }
+}
--- Assets/_Game/Scripts/StateMachines/BattleGameSM/EnemyTurnBattleState.cs
+++ Assets/_Game/Scripts/StateMachines/BattleGameSM/EnemyTurnBattleState.cs
@@ -8,7 +8,16 @@ public class EnemyTurnBattleState : BattleGameState
     public static event Action EnemyTurnBegan;
     public static event Action EnemyTurnEnded;
 
+    CommandStack _commandStack = new CommandStack();
+
     [SerializeField] float _pauseDuration = 1.5f;
+
+    [Header("My Animation and Stuff")]
+    [SerializeField] Animation myAnimation = null;
+
+    [Header("Attack Stats")]
+    public int hitDC = 30;
+    public int attackDamage = 40;
 
     int actionRoll = 0;
 
@@ -29,7 +38,9 @@ public class EnemyTurnBattleState : BattleGameState
     {
         Debug.Log("Enemy thinking...");
         //CHOOSE RANDOM ACTION: ATTACK/HEAL
+
         actionRoll = UnityEngine.Random.Range(1, 3);
+        attackDamage += UnityEngine.Random.Range(1, 30);
 
         yield return new WaitForSeconds(pauseDuration);
 
@@ -38,12 +49,18 @@ public class EnemyTurnBattleState : BattleGameState
         if (actionRoll == 1)
         {
             //ATTACK
+            int myRoll = UnityEngine.Random.Range(1, 101);
+            _commandStack.ExecuteCommand(new Attack_Player(myAnimation, myRoll, hitDC, attackDamage));
+
         }
         if (actionRoll == 2)
         {
             //HEAL
+            Debug.Log("Heal!");
         }
         //PLAY ACTION ANIMATION
+
+        yield return new WaitForSeconds(pauseDuration);
         EnemyTurnEnded?.Invoke();
         //turn over, go back to Player.
         StateMachine.ChangeState<PlayerTurnBattleState>();
