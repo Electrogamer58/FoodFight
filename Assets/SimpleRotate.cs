f83252baafb6e8442e7a12cc77a863efcd319515
Daniel <dancoches@gmail.com>
2021-12-02 00:41:15 -0600

    Well, okay. Maybe a final little touch

---
+++ Assets/SimpleRotate.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleRotate : MonoBehaviour
+{
+    public float speed;
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(speed, speed, speed) * Time.deltaTime);
+    }
+}
